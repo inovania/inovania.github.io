2c5cc3a746eee5e54da4a193fd67a6e92581eefe
inovania <57197729+inovania@users.noreply.github.com>
2019-10-31 14:36:47 -0600

    Delete peticionesAjax.php

--- pages/masterContent/php/peticionesAjax.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	if (isset($_POST['q'])) {
-	    $str = $_POST['q'];
-	    echo "The string: '<i>".$str."</i>' contains ". strlen($str). ' characters and '. str_word_count($str, 0). ' words.<br>';
-	}
- ?>
